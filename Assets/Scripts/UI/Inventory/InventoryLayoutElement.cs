afcc63e3560bcccf05e7e159b4e995617fcccd68
dmitryh2004 <dhohlinov@mail.ru>
2025-07-25 17:10:03 +0300

    temp 20

---
+++ Assets/Scripts/UI/Inventory/InventoryLayoutElement.cs
@@ -0,0 +1,75 @@
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+
+public class InventoryLayoutElement : MonoBehaviour
+{
+    [SerializeField] LootCategoryManager lootCategoryManager;
+    [SerializeField] List<InventoryItem> itemElements = new ();
+    [SerializeField] int offsetStep;
+    [Space()]
+    [SerializeField] bool hasVolume;
+    [SerializeField] TMP_Text totalVolume;
+    [SerializeField] ProgressBar volumePB;
+    [Space()]
+    [SerializeField] bool hasEstimateCost;
+    [SerializeField] TMP_Text estimateCost;
+    [SerializeField] Sprite unknownSprite;
+
+    int activeItemID = -1;
+    int offset = 0;
+
+    public void UpdateLayout(Dictionary<int, int> items)
+    {
+        int itemIndex = itemElements.Count - 1 + offset;
+        int totalCost = 0;
+        foreach (int i in items.Keys)
+        {
+            LootCategory lc = lootCategoryManager.lootCategories.FirstOrDefault((x) => x.id == i);
+
+            if (itemIndex < itemElements.Count)
+            {
+                itemElements[itemIndex].SetActive(true);
+
+                Sprite itemSprite = lc.sprite;
+                if (itemSprite == null) itemSprite = unknownSprite;
+
+                itemElements[itemIndex].Initialize(i, itemSprite, items[i]);
+                itemElements[itemIndex].UpdateTooltip(lc);
+            }
+
+            itemIndex--;
+
+            if (itemIndex < 0) break;
+        }
+
+        for (; itemIndex >= 0; itemIndex--)
+        {
+            itemElements[itemIndex].SetActive(false);
+        }
+
+        //UpdateActiveItem();
+
+        if (hasVolume)
+        {
+            float currentVolume = InventorySystem.Instance.GetOccupiedVolume();
+            float maxVolume = InventorySystem.Instance.GetMaxVolume();
+
+            float ratio = currentVolume / maxVolume * 100;
+            string format = (ratio < 10f) ? "0.0" : ((ratio < 100f) ? "00.0" : "000");
+
+            totalVolume.text = $"{NumberFormatter.FormatNumber(currentVolume * 1000)} / {NumberFormatter.FormatNumber(maxVolume * 1000)} л ({ratio.ToString(format)}%)";
+            volumePB.SetMaxValue(maxVolume);
+            volumePB.SetProgress(currentVolume);
+        }
+        
+        if (hasEstimateCost)
+        {
+            if (totalCost < 2_000_000_000)
+                estimateCost.text = $"ќценочна€ стоимость вещей: {NumberFormatter.FormatNumberWithGrouping(totalCost)} руб.";
+            else
+                estimateCost.text = $"ќценочна€ стоимость вещей: более 2 млрд руб.";
+        }
+    }
+}
